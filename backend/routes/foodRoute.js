f01d6da4f4597efd6ecbe422668cdf417a92fe03
pammu453 <pramodsavant2001@gmail.com>
2024-07-01 22:49:02 +0530

    ./uploads

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -2,28 +2,17 @@ import express from 'express'
 import { addFoodItem, getAllFoodItems, deleteFood } from '../controllers/foodController.js'
 import multer from 'multer'
 
-import fs from 'fs';
-import path from 'path';
-
 const router = express.Router()
 
-const __dirname = path.resolve()
-
 // image storage Engine
-const uploadDir = path.join(__dirname, '../../backend/uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-// Image storage engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir);
+        cb(null, '../uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+        cb(null, `${Date.now()}${file.originalname}`)
     }
-});
+})
 
 const upload = multer({ storage: storage })
 
