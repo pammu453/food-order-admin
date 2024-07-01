404c29ecd146d10944e1e92015d15d4f51de57e6
pammu453 <pramodsavant2001@gmail.com>
2024-07-01 22:22:25 +0530

    Bug fixx

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -7,7 +7,7 @@ const router = express.Router()
 // image storage Engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, '/backend/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}${file.originalname}`)
